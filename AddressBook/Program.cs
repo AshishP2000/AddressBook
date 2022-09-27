d348f85db64176a5587af2ddb24c8fd1276f27dd
Ashish <ashish2000patil@gmail.com>
2022-09-27 07:33:09 +0530

    [Ashish]Added: Ability to create Contatcs UC-1

---
+++ AddressBook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-1 Ability to create a Contacts
+            Console.WriteLine("Welcome to my Address Book");
+
+        }
+    }
+}
